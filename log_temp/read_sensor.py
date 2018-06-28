4f03132e11fc45ec7d27de35096f9e15bc16a19c
Kyle MacNeney <eazymac25@yahoo.com>
2018-06-27 22:25:08 -0400

    fixing poll service

--- log_temp/read_sensor.py
+++ log_temp/read_sensor.py
@@ -2,10 +2,11 @@ from datetime import datetime
 from time import sleep
 
 import requests
+import json
 import Adafruit_DHT as dht # easy package to deal with reading GPIO
 
 SENSOR = dht.DHT11
-SLEEP_TIME = 5*60 # let's post the temperature and humidity every 10 minutes
+SLEEP_TIME = 2*60 # let's post the temperature and humidity every 2 minutes
 # works out to be 144 post requests every 12 hrs... I think we can live with that
 
 url = 'http://eazymac25.pythonanywhere.com/rest/api/1/record'
@@ -27,10 +28,12 @@ def poll_sensor(pin=4):
             "ts": current_time
         }
 
-        requests.post(
+        response = requests.post(
             url=url,
-            data=data,
+            data=json.dumps(data),
             headers=headers
         )
 
         sleep(SLEEP_TIME)
+
+poll_sensor()
