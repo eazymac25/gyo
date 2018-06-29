f7beff65944ed00ea1228053cb920a3e7c92b51e
Kyle MacNeney <eazymac25@yahoo.com>
2018-06-29 11:16:23 -0400

    fixed typo

--- gyo_dashboard/api/util/date_parse.py
+++ gyo_dashboard/api/util/date_parse.py
@@ -13,10 +13,10 @@ class DateParse(object):
     def __init__(self):
         self.token_rgx = '|'.join('(?P<%s>%s)' % named_group for named_group in self.tokens)
 
-    def tokenize(self, tag):
+    def tokenize(self, inpt):
         col_num = 0
 
-        for match_obj in re.finditer(self.token_rgx, tag):
+        for match_obj in re.finditer(self.token_rgx, inpt):
             kind = match_obj.lastgroup
             value = match_obj.group(kind)
             if kind == 'MISMATCH':
