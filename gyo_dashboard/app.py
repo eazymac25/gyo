e69b8589c646b742ddf2eee65bde039bc64663f8
Kyle MacNeney <eazymac25@yahoo.com>
2018-06-29 10:55:26 -0400

    adding static and templates

--- gyo_dashboard/app.py
+++ gyo_dashboard/app.py
@@ -1,13 +1,15 @@
 """
 Simple flask app for logging temperature data from raspberry pi
 """
-import re
 
-from flask import Flask, jsonify, request
-import mysql.connector as mysql
+from flask import Flask, jsonify, request, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello from Flask!'
+
+# this is a circular import but unused
+# it allows us to expose the sensor api into the main flask application
+from flask.api import sensor_api
