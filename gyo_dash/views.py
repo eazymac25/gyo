5cffab2cf96bbf46db7438e363f647534be412a1
Kyle MacNeney <eazymac25@yahoo.com>
2018-06-30 18:05:49 -0400

    removing circular ref issue

---
+++ gyo_dash/views.py
@@ -0,0 +1,10 @@
+"""
+Simple flask app for logging temperature data from raspberry pi
+"""
+from gyo_dash import app
+from flask import url_for, render_template
+
+
+@app.route('/')
+def index():
+    return render_template('base.html')
