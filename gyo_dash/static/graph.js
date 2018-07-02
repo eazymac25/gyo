7e57c356e8b5794622cf70f707313c29589ff94a
Kyle MacNeney <eazymac25@yahoo.com>
2018-07-02 16:22:43 -0400

    adding javascript to update data

--- gyo_dash/static/graph.js
+++ gyo_dash/static/graph.js
@@ -1,9 +1,10 @@
-$(window).on('load', function() {
+(function(w, d, $){
+
     var data = null;
     var settings = {
       "async": true,
       "crossDomain": true,
-      "url": "http://eazymac25.pythonanywhere.com/rest/api/1/measureHistory?timeframe=30&period=M&startAt=0&maxResults=100",
+      "url": null,
       "method": "GET",
       "headers": {
         "content-type": "application/json",
@@ -12,23 +13,89 @@ $(window).on('load', function() {
       "processData": false,
     }
 
-    $.ajax(settings).done(function (response) {
-      data = response;
-    });
-    console.log(data.maxResults);
-    var humidity = data.measureHistory.records.map(obj => obj.humidity);
-    var dates = data.measureHistory.records.map(obj => obj.createTime);
+    function getMeasurements(timeframe, period){
 
-    var ctx = $("#myChart");
-    var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: dates,
-            datasets: [{
-                label: 'Humidity',
-                data: humidity,
-            }]
-        },
-        options: {}
+        settings.url = "http://eazymac25.pythonanywhere.com/rest/api/1/measureHistory?timeframe=" + timeframe + 
+        "&period=" + period +  "&startAt=0&maxResults=1000";
+
+        $.ajax(settings).done(function (response){
+            createChart(response);
+        });
+    }
+
+    function createChart(data){
+
+        console.log('hello');
+        console.log(data.measureHistory.max_results);
+
+        var humidity = data.measureHistory.records.map(obj => obj.humidity);
+        var temperature = data.measureHistory.records.map(obj => obj.temperature);
+        var dates = data.measureHistory.records.map(obj => obj.createTime);
+
+        var ctx = $("#myChart");
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: dates,
+                datasets: [{
+                    label: 'Humidity',
+                    data: humidity,
+                    borderColor: "#3e95cd",
+                    fill: false
+                },
+                {
+                    label: 'Temperature',
+                    data: temperature,
+                    borderColor: "#c45850",
+                    fill: false
+                }]
+            },
+            options: {
+                title: {
+                    text: "Humidity Over Time",
+                    display: true
+                },
+                // scales: {
+                //     yAxes: [{
+                //         ticks: {
+                //             beginAtZero: true
+                //         }
+                //     }]
+                // }
+            }
+        });
+    }
+
+    function textToPeriod(text){
+        if (text.toLowerCase().includes("min")){
+            return "M";
+        }
+        if (text.toLowerCase().includes("hr")){
+            return "H";
+        }
+        if (text.toLowerCase().includes("day")){
+            return "D";
+        }
+        if (text.toLowerCase().includes("week")){
+            return "W";
+        }
+        return null;
+    }
+
+    $(d).ready(function(){
+        $('button').on('click', function() {
+
+            var temp = this.textContent.split(" ");
+            var timeframe = temp[0];
+            var period = textToPeriod(temp[1]);
+
+            console.log(this.textContent);
+
+            getMeasurements(timeframe, period);
+        });
     });
-});
+
+    // load default
+    getMeasurements("30", "M");
+
+})(window, document, $);
