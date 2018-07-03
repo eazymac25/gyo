3fceba02bea911227890a6809dee8153a7c7f9b5
Kyle MacNeney <eazymac25@yahoo.com>
2018-07-03 15:09:00 -0400

    updating to two graphs

--- gyo_dash/static/graph.js
+++ gyo_dash/static/graph.js
@@ -1,6 +1,8 @@
 (function(w, d, $){
 
-    var sensorGraph = null;
+    var hChart = null;
+    var tChart = null;
+
     var data = null;
     var settings = {
       "async": true,
@@ -16,7 +18,7 @@
 
     function getMeasurements(timeframe, period){
 
-        settings.url = "http://eazymac25.pythonanywhere.com/rest/api/1/measureHistory?timeframe=" + timeframe + 
+        settings.url = "http://eazymac25.pythonanywhere.com/rest/api/1/measureHistory?timeframe=" + timeframe +
         "&period=" + period +  "&startAt=0&maxResults=1000";
 
         $.ajax(settings).done(function (response){
@@ -25,54 +27,95 @@
     }
 
     function createChart(data){
-
-        console.log('hello');
+        
         console.log(data.measureHistory.max_results);
 
         var humidity = data.measureHistory.records.map(obj => obj.humidity);
         var temperature = data.measureHistory.records.map(obj => obj.temperature);
         var dates = data.measureHistory.records.map(obj => obj.createTime);
 
-        if (sensorGraph == null) {
-            var ctx = $("#myChart");
-            sensorGraph = new Chart(ctx, {
+        if (hChart == null) {
+            var hctx = $("#humidityChart");
+            var tctx = $("#temperatureChart");
+            hChart = new Chart(hctx, {
                 type: 'line',
                 data: {
                     labels: dates,
                     datasets: [{
                         label: 'Humidity',
                         data: humidity,
+                        yAxisID: 'H',
                         borderColor: "#3e95cd",
-                        fill: false
-                    },
-                    {
-                        label: 'Temperature',
-                        data: temperature,
-                        borderColor: "#c45850",
                         fill: false
                     }]
                 },
                 options: {
                     title: {
                         text: "Humidity Over Time",
-                        fontSize: 16,
+                        fontSize: 24,
                         display: true
                     },
-                    // scales: {
-                    //     yAxes: [{
-                    //         ticks: {
-                    //             beginAtZero: true
-                    //         }
-                    //     }]
-                    // }
+                    scales: {
+                        yAxes: [{
+                            id: 'H',
+                            type: 'linear',
+                            position: 'left',
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Relative Humidity (%)',
+                                fontSize: 16
+                            }
+                        }]
+                    }
+                }
+            });
+
+            tChart = new Chart(tctx, {
+                type: 'line',
+                data: {
+                    labels: dates,
+                    datasets: [{
+                        label: 'Temperature',
+                        data: temperature,
+                        yAxisID: 'T',
+                        borderColor: "#c45850",
+                        fill: false
+                    }]
+                },
+                options: {
+                    title: {
+                        text: "Temperature Over Time",
+                        fontSize: 24,
+                        display: true
+                    },
+                    scales: {
+                        yAxes: [{
+                            id: 'T',
+                            type: 'linear',
+                            position: 'left',
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Temperature (C)',
+                                fontSize: 16
+                            },
+                            ticks: {
+                                min: 18,
+                                max: 30
+                            }
+                        }]
+                    }
                 }
             });
 
         } else {
-            sensorGraph.data.labels = dates;
-            sensorGraph.data.datasets[0].data = humidity;
-            sensorGraph.data.datasets[1].data = temperature;
-            sensorGraph.update(0);
+            hChart.data.labels = dates;
+            hChart.data.datasets[0].data = humidity;
+
+            tChart.data.labels = dates;
+            tChart.data.datasets[0].data = temperature;
+
+            hChart.update(0);
+            tChart.update(0);
         }
     }
 
@@ -98,8 +141,6 @@
             var temp = this.textContent.split(" ");
             var timeframe = temp[0];
             var period = textToPeriod(temp[1]);
-
-            console.log(this.textContent);
 
             getMeasurements(timeframe, period);
         });
